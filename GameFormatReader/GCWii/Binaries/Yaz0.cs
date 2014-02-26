c8f07219e6f49750eea8c653b5767456568cc07b
Lioncash <mathew1800@gmail.com>
2014-02-26 07:52:26 -0500

    Add Yaz0 decompression support.

---
+++ GameFormatReader/GCWii/Binaries/Yaz0.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using GameFormatReader.Common;
+
+namespace GameFormatReader.GCWii.Binaries
+{
+	/// <summary>
+	/// Class for decoding the Yaz0 compression format.
+	/// </summary>
+	public static class Yaz0
+	{
+		// Position where the compressed data starts.
+		private const int StartOfData = 0x10;
+
+		/// <summary>
+		/// Decodes the given source data.
+		/// </summary>
+		/// <remarks>Decoding alg borrowed from YAGCD.</remarks>.
+		/// <param name="input">The source data to decode.</param>
+		/// <returns>The decoded data.</returns>
+		public static byte[] Decode(byte[] input)
+		{
+			using (EndianBinaryReader reader = new EndianBinaryReader(new MemoryStream(input), Endian.BigEndian))
+			{
+				// Check if the data is even Yaz0 compressed.
+				if (new string(reader.ReadChars(4)) != "Yaz0")
+					throw new ArgumentException("input is not Yaz0 compressed data", "input");
+
+				// These 4 bytes read tell us the decompressed data size.
+				byte[] output = new byte[reader.ReadUInt32()];
+
+				// Now decode the compressed data.
+				long srcPos = StartOfData;
+				long dstPos = 0;
+				uint validBitCount = 0; // Number of valid bits left in the 'code' byte.
+				byte currentCodeByte = 0;
+
+				while (dstPos < output.Length)
+				{
+					if (validBitCount == 0)
+					{
+						currentCodeByte = input[srcPos++];
+						validBitCount = 8;
+					}
+
+					if ((currentCodeByte & 0x80) != 0)
+					{
+						// Straight copy
+						output[dstPos++] = input[srcPos++];
+					}
+					else
+					{
+						// RLE part.
+						byte byte1 = input[srcPos++];
+						byte byte2 = input[srcPos++];
+
+						uint dist     = (uint) (((byte1 & 0x0F) << 8) | byte2);
+						uint copySrc  = (uint) (dstPos - (dist + 1));
+						uint numBytes = (uint) (byte1 >> 4);
+
+						if (numBytes == 0)
+						{
+							numBytes = input[srcPos++] + 0x12U;
+						}
+						else
+						{
+							numBytes += 2;
+						}
+
+						// Copy run
+						for (int i = 0; i < numBytes; i++)
+						{
+							output[dstPos++] = output[copySrc++];
+						}
+					}
+
+					// Use next bit from the "code" byte.
+					currentCodeByte <<= 1;
+					validBitCount -= 1;
+				}
+
+				return output;
+			}
+		}
+	}
+}
